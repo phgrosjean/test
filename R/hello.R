c8f47882e9c76805b31f6420a70a71d7ed190d41
phgrosjean <phgrosjean@sciviews.org>
2023-12-14 07:57:19 +0100

    Roxygen doc + proper MIT license

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,17 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
+#' Print "Hello world!" and return TRUE invisibly.
+#'
+#' @return Always returns `TRUE` invisibly.
+#' @export
+#'
+#' @examples
+#' hello()
 hello <- function() {
+  if (FALSE) {
+    cat("Code never executed")
+  }
   print("Hello, world!")
+  invisible(TRUE)
 }
--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -1,5 +1,6 @@
 test_that("multiplication works", {
   expect_equal(2 * 2, 4)
-  expect_true(1 > 2)
+  expect_true(2 > 1)
   expect_error(log("a"))
+  expect_true(hello())
 })
